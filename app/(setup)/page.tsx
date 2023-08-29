aadd6121524a1951674eb8f1f2aeed3d4b120bd3
Himanshu Singh <chamyalhemsingh@gmail.com>
2023-08-30 00:41:28 +0530

    Frontend- Built a create server form

--- app/(setup)/page.tsx
+++ app/(setup)/page.tsx
@@ -1,6 +1,7 @@
 import { redirect } from "next/navigation";
 import { initialProfile } from "@/lib/initial-profile";
 import { db } from "@/lib/db";
+import { InitialModal } from "@/components/modals/initial-modal";
 const SetupPage = async () => {
   const profile = await initialProfile();
 
@@ -17,7 +18,7 @@ const SetupPage = async () => {
   if (server) {
     return redirect(`/servers/${server.id}`);
   }
-  return <div>Create a server</div>;
+  return <InitialModal />;
 };
 
 export default SetupPage;
