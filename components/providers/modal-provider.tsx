eba38bc642bf7affb3e12a4875f47b406b870bd5
Himanshu Singh <chamyalhemsingh@gmail.com>
2023-09-04 17:40:39 +0530

    Frontend+Backend: Created the server settings modal and its functionality

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -5,6 +5,7 @@ import { useEffect, useState } from "react";
 import { CreateServer } from "@/components/modals/create-server";
 import { InviteModal } from "@/components/modals/invite-modal";
 import { DeleteServerModal } from "@/components/modals/delete-server-modal";
+import { EditServerModal } from "@/components/modals/edit-server-modal";
 
 export const ModalProvider = () => {
   const [isMounted, setIsMounted] = useState(false);
@@ -16,6 +17,7 @@ export const ModalProvider = () => {
     <>
       <CreateServer />
       <InviteModal />
+      <EditServerModal />
       <DeleteServerModal />
     </>
   );
--- hooks/use-modal-store.ts
+++ hooks/use-modal-store.ts
@@ -1,7 +1,11 @@
 import { Server } from "@prisma/client";
 import { create } from "zustand";
 
-export type ModalType = "createServer" | "invite" | "deleteServer";
+export type ModalType =
+  | "createServer"
+  | "invite"
+  | "deleteServer"
+  | "editServer";
 
 interface ModalData {
   server?: Server;
