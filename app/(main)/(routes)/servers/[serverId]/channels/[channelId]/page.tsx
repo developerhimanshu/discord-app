981496b868f54c5e998ff965f8a0a7f32a5d6a85
Himanshu Singh <chamyalhemsingh@gmail.com>
2023-09-20 23:03:35 +0530

    FrontEnd: Built Chat header

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,9 +1,46 @@
-"use client";
-import { useParams, useRouter } from "next/navigation";
+import { currentProfile } from "@/lib/current-profile";
+import { redirectToSignIn } from "@clerk/nextjs";
+import { db } from "@/lib/db";
+import { redirect } from "next/navigation";
+import { ChatHeader } from "@/components/chat/chat-header";
 
-const ChannelIdPage = () => {
-  const params = useParams();
-  return <div>ChannelIdPage!{params?.channelId}</div>;
+interface channelIdPageProps {
+  params: {
+    serverId: string;
+    channelId: string;
+  };
+}
+
+const ChannelIdPage = async ({ params }: channelIdPageProps) => {
+  const profile = await currentProfile();
+
+  if (!profile) return redirectToSignIn();
+
+  const channel = await db.channel.findUnique({
+    where: {
+      id: params.channelId,
+    },
+  });
+
+  const member = await db.member.findFirst({
+    where: {
+      serverId: params.serverId,
+      profileId: profile?.id,
+    },
+  });
+
+  if (!channel || !member) {
+    redirect("/");
+  }
+  return (
+    <div className="bg-white dark:bg-[#313338] flex flex-col h-full ">
+      <ChatHeader
+        name={channel.name}
+        serverId={channel.serverId}
+        type={"channel"}
+      />
+    </div>
+  );
 };
 
 export default ChannelIdPage;
